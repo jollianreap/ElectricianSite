3d75d8c947b4fb549521b1586b395c9f2e8c621e
v1ntex <leonidt668@gmail.com>
2022-08-18 16:19:00 +0300

    renamed directory

--- electrician/electrician/asgi.py
+++ electrician/electrician/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for elctrician project.
+ASGI config for electrician project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elctrician.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'electrician.settings')
 
 application = get_asgi_application()
--- electrician/electrician/wsgi.py
+++ electrician/electrician/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for elctrician project.
+WSGI config for electrician project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elctrician.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'electrician.settings')
 
 application = get_wsgi_application()
